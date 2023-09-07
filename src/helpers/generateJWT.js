94de5d89a5f40afc9fea82cd6dfcf49633fd5b06
Rober <roberdz30@gmail.com>
2023-09-06 23:38:06 -0300

    Commit with helpers

---
+++ src/helpers/generateJWT.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+
+/* Cadena codificada */
+const generateJWT = (id) => {
+
+    const secret = process.env.JWT_SECRET;
+
+    return jwt.sign({id}, secret , {
+        expiresIn: '15d',
+    })
+}
+
+module.exports = {
+    generateJWT
+}
