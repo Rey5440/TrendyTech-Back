5320c7a05b61fb3c336cc5469d589c776b3b6877
F.Fernandez <hellsingsd@gmail.com>
2023-09-15 16:50:44 -0300

    con rutas /types, /colors, /brands

---
+++ src/controllers/brandsControllers/getAllBrands.js
@@ -0,0 +1,14 @@
+const { Brand } = require("../../db/db");
+
+const getAllBrands = async () => {
+    try {
+        const brands = await Brand.findAll();
+        console.log(brands)
+        return brands;
+    } catch (error) {
+        console.error("Error al obtener todas las marcas:", error);
+        throw error; // o maneja el error de manera adecuada según tus necesidades
+    }
+};
+
+module.exports = getAllBrands;
