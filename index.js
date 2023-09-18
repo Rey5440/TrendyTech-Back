18400eee9983d1ad98b4f5c72a5a1939e28ca0f5
Lokerrr <juan53@live.com.ar>
2023-09-17 23:40:10 -0300

    last commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db/db.js");
-
+const {getDBinfo} = require('./src/controllers/productsControllers/getProductsController.js')
 conn.sync({ force: true }).then(() => {
-  server.listen(3004, () => {
-    
+  server.listen(3004, async () => {
+    await getDBinfo();
     console.log("Server listening on 3004");
   });
 });
