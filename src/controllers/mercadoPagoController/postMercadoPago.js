7e4972fe24e675b6d4add7492f5630643734e02a
00JAS00 <julian.m.sosa.muchut@gmail.com>
2023-09-21 23:29:14 -0300

    muchos productos a mercado pago

--- src/controllers/mercadoPagoController/postMercadoPago.js
+++ src/controllers/mercadoPagoController/postMercadoPago.js
@@ -6,20 +6,23 @@ mercadopago.configure({
     access_token: MP_ACCESS_TOKEN,
 });
 
-const postMercadoPago =async (name, price, quantity)=>{
+const postMercadoPago =async (productos)=>{
     let preference={
-        items:[
-            {
-                title: name,
-                unit_price: Number(price),
-                quantity: Number(quantity)
-            }
-        ],
+        items:productos,
         back_urls:{
             success: "http://localhost:5173/",// caso exitoso ruta front (al mismo producto o a mis compras x ejemplo)
             failure: "http://localhost:5173/",// caso fallido ruta front (un mensaje de error o similar)
             pending: "" // caso pendiente(en efectivo)
         },
+        payment_methods: {
+            excluded_payment_types: [
+                {
+                    id: "ticket"
+                }
+            ],
+            installments: 6
+            },
+        
         auto_return: "approved",
     };
     mercadopago.preferences.create(preference)
