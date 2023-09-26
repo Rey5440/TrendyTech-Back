83e4f3d038f11040088911493d375ca2d426c3a0
Facundo Fernandez <HellsingSD@gmail.com>
2023-09-26 14:33:05 -0300

    Update index.js
    true

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db/db.js");
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3004, () => {
     console.log("Server listening on 3004");
   });
