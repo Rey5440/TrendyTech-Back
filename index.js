e9d66a5253792a26d5b380f13d2bbf1546ad461f
Tomas Callenius <112117523+tomascallenius@users.noreply.github.com>
2023-09-27 10:08:59 -0300

    false index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db/db.js");
 const {getDBinfo} = require('./src/controllers/productsControllers/getProductsController.js')
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3004, async() => {
         await getDBinfo();
     console.log("Server listening on 3004");
