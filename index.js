9c7386c6332e15dd969924836f7e582b3c1f9225
Tomas Callenius <112117523+tomascallenius@users.noreply.github.com>
2023-09-27 09:44:53 -0300

    getdbinfo() index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db/db.js");
+const {getDBinfo} = require('./src/controllers/productsControllers/getProductsController.js')
 
 conn.sync({ force: false }).then(() => {
   server.listen(3004, () => {
+        await getDBinfo();
     console.log("Server listening on 3004");
   });
 });
