961cafb502a9e857ba58406ac90b3bb1b690088c
tomascallenius <tomascallenius96@gmail.com>
2023-09-29 23:50:13 -0300

    the cookies are back

--- src/controllers/userController/createUserAuth0.js
+++ src/controllers/userController/createUserAuth0.js
@@ -1,25 +1,23 @@
-const { User } = require('../../db/db.js')
+const { User } = require("../../db/db.js");
 
 const createAuth0User = async (auth0UserData) => {
-
-    const existingUser = await User.findOne({ where: { email: auth0UserData.email } })
-
-    if (existingUser) {
-        throw new Error("Usuario Auth0 ya existe en la Database");
-    };
-
-    const user = await User.create({ //find or create
-        name: auth0UserData.name,
-        email: auth0UserData.email,
-        password: auth0UserData.sub,
-        token: auth0UserData.sub,
-        image: auth0UserData.picture
-
-    })
-
+  const existingUser = await User.findOne({
+    where: { token: auth0UserData.token },
+  });
+  if (existingUser) {
+    return existingUser;
+  } else {
+    const user = await User.create({
+      name: auth0UserData.name,
+      email: auth0UserData.email,
+      password: auth0UserData.sub,
+      token: auth0UserData.token,
+      image: auth0UserData.picture,
+    });
     return user;
-}
+  }
+};
 
 module.exports = {
-    createAuth0User
-}
+  createAuth0User,
+};
