127466f413be2c4468186fbde7dd947bfabe7e4e
Lokerrr <juan53@live.com.ar>
2023-10-03 22:07:36 -0300

    emailToBuy

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -3,37 +3,37 @@ require("dotenv").config();
 const { google } = require("googleapis");
 
 const emailRegister = async (data) => {
-    const { email, name, token } = data;
-  
-    // Se crea un objeto transport utilizando nodemailer.createTransport(). Este objeto se configura con los datos de autenticación del servidor de correo electrónico (host, puerto, usuario y contraseña) que se obtienen de las variables de entorno (process.env).
-    const transport = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false
-      }
-    });
-  
-    //Informacion del email
-  
-    // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/confirmar/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda confirmar su cuenta haciendo clic en él.                <p>Hola: ${name}, confirma tu cuenta en Trendy Tech</p>
-    // <p>Tu cuenta ya esta casi lista, solo debes comprobarla haciendo click en el siguiente enlace</p>
+  const { email, name, token } = data;
 
-    // <a href="${process.env.FRONTEND_URL}/confirm/${token}">Confirmar Cuenta</a>
+  // Se crea un objeto transport utilizando nodemailer.createTransport(). Este objeto se configura con los datos de autenticación del servidor de correo electrónico (host, puerto, usuario y contraseña) que se obtienen de las variables de entorno (process.env).
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-                // <p>Si tu no reconoces este mail, por favor ignora este mensaje</p>
-  
-    const info = await transport.sendMail({
-                from: '"Trendy Tech - Tecnologia de primer nivel" <bienvenidos@trendy-spot.com>',
-                to: email,
-                subject: "Trendy Tech - Confirma tu cuenta",
-                text: "Confirma tu cuenta en Trendy-Tech",
-                html: `       
+  //Informacion del email
+
+  // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/confirmar/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda confirmar su cuenta haciendo clic en él.                <p>Hola: ${name}, confirma tu cuenta en Trendy Tech</p>
+  // <p>Tu cuenta ya esta casi lista, solo debes comprobarla haciendo click en el siguiente enlace</p>
+
+  // <a href="${process.env.FRONTEND_URL}/confirm/${token}">Confirmar Cuenta</a>
+
+  // <p>Si tu no reconoces este mail, por favor ignora este mensaje</p>
+
+  const info = await transport.sendMail({
+    from: '"Trendy Tech - Tecnologia de primer nivel" <bienvenidos@trendy-spot.com>',
+    to: email,
+    subject: "Trendy Tech - Confirma tu cuenta",
+    text: "Confirma tu cuenta en Trendy-Tech",
+    html: `       
                 <p>Hola: ${name}, confirma tu cuenta en Trendy Tech</p> 
                 <body style="word-spacing:normal;background-color:#F4F4F4;">
                 <div style="background-color:#F4F4F4;">
@@ -179,35 +179,35 @@ const emailRegister = async (data) => {
 
 
               `,
-    });
-  };
-  
-  const emailForgetPassword = async (data) => {
-    const { email, name, token } = data;
-  
-    // Se crea un objeto transport utilizando nodemailer.createTransport(). Este objeto se configura con los datos de autenticación del servidor de correo electrónico (host, puerto, usuario y contraseña) que se obtienen de las variables de entorno (process.env).
-    const transport = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false
-      }
-    });
-  
-    //Informacion del email
-    // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/Recuperar Contraseña/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda Recuperar Contraseña su cuenta haciendo clic en él.
-  
-    const info = await transport.sendMail({
-                from: '"Trendy Tech - Tienda de ropa" <cuentas@trendy-tech.com>',
-                to: email,
-                subject: "Trendy Tech - Recupera tu cuenta",
-                text: "Recupera tu cuenta en Trendy-Tech",
-                html: `<p>Hola: ${name}, recupera tu cuenta en Trendy Tech</p>
+  });
+};
+
+const emailForgetPassword = async (data) => {
+  const { email, name, token } = data;
+
+  // Se crea un objeto transport utilizando nodemailer.createTransport(). Este objeto se configura con los datos de autenticación del servidor de correo electrónico (host, puerto, usuario y contraseña) que se obtienen de las variables de entorno (process.env).
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  //Informacion del email
+  // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/Recuperar Contraseña/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda Recuperar Contraseña su cuenta haciendo clic en él.
+
+  const info = await transport.sendMail({
+    from: '"Trendy Tech - Tienda de ropa" <cuentas@trendy-tech.com>',
+    to: email,
+    subject: "Trendy Tech - Recupera tu cuenta",
+    text: "Recupera tu cuenta en Trendy-Tech",
+    html: `<p>Hola: ${name}, recupera tu cuenta en Trendy Tech</p>
                 <body style="word-spacing:normal;background-color:#F4F4F4;">
                 <div style="background-color:#F4F4F4;">
                   <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" bgcolor="#000000" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
@@ -351,10 +351,43 @@ const emailRegister = async (data) => {
               </body>
 
             `,
-    });
-  };
-  
-  module.exports = {
-    emailRegister,
-    emailForgetPassword,
-  };
+  });
+};
+
+const emailConfirmBuy = async (data) => {
+  const { email, name } = data;
+
+  // Se crea un objeto transport utilizando nodemailer.createTransport(). Este objeto se configura con los datos de autenticación del servidor de correo electrónico (host, puerto, usuario y contraseña) que se obtienen de las variables de entorno (process.env).
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  //Informacion del email
+  // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/confirmar/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda confirmar su cuenta haciendo clic en él.
+
+  const info = await transport.sendMail({
+    from: '"Trendy Tech " <cuentas@trendy-spot.com>',
+    to: email,
+    subject: "Gracias por su compra! Fe no me nal.",
+    text: "Compra en Trendy-Tech",
+    html: `<p>Hola: ${name}, felicidades por tu compra!</p>
+
+              <p>Si no solicitaste esta petición ignora este mensaje</p>
+          `,
+  });
+};
+
+module.exports = {
+  emailRegister,
+  emailForgetPassword,
+  emailConfirmBuy,
+};
