12ea9c39fc64dc3884616d8ef31afad22efcc8a1
Rober <roberdz30@gmail.com>
2023-10-04 03:04:43 -0300

    commit para actualizar con juan

--- src/helpers/validationUpdateHelpers.js
+++ src/helpers/validationUpdateHelpers.js
@@ -2,7 +2,6 @@ const validatePrice = (price) => {
     const errors = {};
 
     if (!price) errors.price = "Debe insertar un precio";
-    if (price < 100 || price > 200000) errors.price = "El precio debe estar entre 100 y 200000";
     if (typeof price !== "number") errors.price = "El precio debe ser un número";
 
     return errors;
@@ -12,7 +11,7 @@ const validateDescription = (description) => {
     const errors = {};
 
     if (!description) errors.description = "Debe escribir una descripción del producto";
-    if (description.length < 10 || description.length > 600) errors.description = "La descripción debe contener entre 10 y 600 caracteres";
+    
 
     return errors;
 }
@@ -20,10 +19,10 @@ const validateDescription = (description) => {
 const validateStock = (stock) => {
     const errors = {};
 
-    if (!stock) errors.stock = "Debe ingresar el stock";
+    
     if (typeof stock !== "number") errors.stock = "El stock debe ser un número";
     if (stock < 0) errors.stock = "El valor no puede ser un número negativo";
-    if (stock > 3000) errors.stock = "El stock no puede ser mayor a 3000";
+    
 
     return errors;
 }
