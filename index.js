e4a2fcf3021a14d9985604d63f1aeb1824a0e55d
tomascallenius <tomascallenius96@gmail.com>
2024-04-04 10:53:21 -0300

    fix

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const {
   getDBinfo,
 } = require("./src/controllers/productsControllers/getProductsController.js");
 
+const PORT = process.env.PORT;
+
 conn.sync({ force: false }).then(() => {
-  server.listen(3004, async () => {
+  server.listen(PORT, async () => {
     await getDBinfo();
-    console.log("Server listening on 3004");
+    console.log(`Server listening on ${PORT}`);
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
-
 const routes = require('./routes/index.js');
+require('./db/db.js');
+
 const cors = require("cors");
 
-require('./db/db.js');
 
 const server = express();
 
 server.name = "API";
 server.use(cors());
-
 server.use(express.json());
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use("/", routes);
 
